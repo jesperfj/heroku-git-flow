fc3536da85eb4b79c3ce87caed6fe0700ad011bb
Jesper Joergensen <jesper@heroku.com>
2014-07-18 14:00:08 -0700

    Experimental support for streaming output

--- lib/heroku/api/api.rb
+++ lib/heroku/api/api.rb
@@ -12,7 +12,7 @@ module Heroku
         :expects => 202,
         :path => '/app-setups',
         :body => Heroku::OkJson.encode(data),
-        :headers => {'Accept' => 'application/vnd.heroku+json; version=3'}
+        :headers => {'Accept' => "application/vnd.heroku+json; version=#{api_version}"}
         ).body
     end
 
@@ -21,7 +21,7 @@ module Heroku
         :method => :get,
         :expects => 200,
         :path => "/app-setups/#{id}",
-        :headers => {'Accept' => 'application/vnd.heroku+json; version=3'}
+        :headers => {'Accept' => "application/vnd.heroku+json; version=#{api_version}"}
         ).body
     end
 
@@ -32,7 +32,7 @@ module Heroku
         :expects => [201, 503],
         :path => "/apps/#{app}/builds",
         :body => Heroku::OkJson.encode(data),
-        :headers => {'Accept' => 'application/vnd.heroku+json; version=3'}
+        :headers => {'Accept' => "application/vnd.heroku+json; version=#{api_version}"}
         )
       # this is a little messy. But the beginnings of supporting MFA per request
       if response.status == 503
@@ -43,7 +43,7 @@ module Heroku
           :expects => 201,
           :path => "/apps/#{app}/builds",
           :body => Heroku::OkJson.encode(data),
-          :headers => {'Accept' => 'application/vnd.heroku+json; version=3',
+          :headers => {'Accept' => "application/vnd.heroku+json; version=#{api_version}",
                        'Heroku-Two-Factor-Code' => mfa }
           ).body
       else
@@ -56,10 +56,19 @@ module Heroku
         :method => :get,
         :expects => 200,
         :path => "/apps/#{app}/builds/#{id}/result",
-        :headers => {'Accept' => 'application/vnd.heroku+json; version=3'}
+        :headers => {'Accept' => "application/vnd.heroku+json; version=#{api_version}"}
         ).body
     end
 
+    private
+
+      def api_version
+        if ENV["STREAMING_BUILD_RESULT"]
+          "edge"
+        else
+          "3"
+        end
+      end
 
   end
 end
--- lib/heroku/command/deploy.rb
+++ lib/heroku/command/deploy.rb
@@ -1,5 +1,6 @@
 $stdout.sync = true
 require 'tempfile'
+require 'excon'
 
 class Heroku::Command::Deploy < Heroku::Command::Base
 
@@ -17,34 +18,77 @@ class Heroku::Command::Deploy < Heroku::Command::Base
     file = Tempfile.new('heroku')
     begin
       git("archive #{branch} --format tar.gz -o #{file.path}.tar.gz")
+      size = (File.size(file.path+'.tar.gz').to_f/1000000).round(1)
       version = git("rev-parse #{branch}")
-      print("Uploading source tarball for branch #{branch}...")
+      print("Uploading source tarball (#{size} MB) for branch #{branch}...")
       drop = Heroku::HDrop::DropFile.new
       drop.upload(file.path+'.tar.gz')
       puts(" done")
+
       print "Deploying branch #{branch} (commit #{version[0..9]}) to #{app}."
       build = api.start_build(drop.get, app, version)
-      build = api.build_result(app, build['id'])
-      while(build['build']['status'] == 'pending')
-        print('.')
-        sleep(3)
-        build = api.build_result(app, build['build']['id'])
-      end
-      if build['build']['status'] == 'succeeded'
-        puts " done"
-        build['lines'].each { |line|
-          print line['line']
-        }
-        puts "Commit #{version[0..9]} on branch #{branch} now running on #{app}"
-        puts
+
+      if ENV['STREAMING_BUILD_RESULT']
+        streaming_build_output(build,version,branch,app)
       else
-        puts " failed"
-        puts build['failure_message']
+        nonstreaming_build_output(build,version,branch,app)
       end
     ensure
       file.close
       file.unlink
     end
   end
+
+  private
+
+    def streaming_build_output(build, version, branch, app)
+      puts
+      streamer = lambda do |chunk, remaining_bytes, total_bytes|
+        puts chunk
+      end
+
+      res = Excon.get(build['output_stream_url'],
+        response_block: streamer
+      )
+
+      build_result = api.build_result(app, build['id'])
+      # build should be done but checking just in case
+      while(build_result['build']['status'] == 'pending')
+        sleep(3)
+        build_result = api.build_result(app, build_result['build']['id'])
+      end
+
+      if build_result['build']['status'] == 'succeeded'
+        puts "Build successful."
+        puts
+        puts "Commit #{version[0..9]} on branch #{branch} now running on #{app}"
+        puts
+      else
+        puts "Build failed!"
+        puts build_result['failure_message']
+      end
+    end
+
+    def nonstreaming_build_output(build, version, branch, app)
+      build_result = api.build_result(app, build['id'])
+      while(build_result['build']['status'] == 'pending')
+        print('.')
+        sleep(3)
+        build_result = api.build_result(app, build_result['build']['id'])
+      end
+      if build_result['build']['status'] == 'succeeded'
+        puts " done"
+        build_result['lines'].each { |line|
+          print line['line']
+        }
+        puts "Commit #{version[0..9]} on branch #{branch} now running on #{app}"
+        puts
+      else
+        puts " failed"
+        puts build_result['failure_message']
+      end
+
+    end
+
 
 end
